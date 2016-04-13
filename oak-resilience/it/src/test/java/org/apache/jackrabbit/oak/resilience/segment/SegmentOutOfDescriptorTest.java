3359daf1c48d970920270a6bf570f346b953f509
Tomasz Rekawek <tomekr@apache.org>
2016-04-13 10:02:13 +0200

    Basic test for the descriptor exhaustion.

---
+++ oak-resilience/it/src/test/java/org/apache/jackrabbit/oak/resilience/segment/SegmentOutOfDescriptorTest.java
@@ -0,0 +1,48 @@
+package org.apache.jackrabbit.oak.resilience.segment;
+
+import org.apache.jackrabbit.oak.resilience.remote.segment.TreeNodeWriter;
+import org.apache.jackrabbit.oak.resilience.vagrant.RemoteJar;
+import org.apache.jackrabbit.oak.resilience.vagrant.RemoteJvmProcess;
+import org.apache.jackrabbit.oak.resilience.vagrant.VagrantVM;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertEquals;
+
+public class SegmentOutOfDescriptorTest {
+    private static final Map<String, String> PROPS = Collections.singletonMap("OAK_DIR",
+            "/home/vagrant/" + SegmentOutOfDescriptorTest.class.getName());
+
+    private VagrantVM vm;
+
+    private RemoteJar itJar;
+
+    @Before
+    public void setupVm() throws IOException {
+        vm = new VagrantVM.Builder().setVagrantFile("src/test/resources/Vagrantfile").build();
+        vm.init();
+        vm.start();
+        itJar = vm.uploadJar("org.apache.jackrabbit", "oak-resilience-it-remote", "1.6-SNAPSHOT");
+    }
+
+    @After
+    public void destroyVm() throws IOException {
+        vm.stop();
+        vm.destroy();
+    }
+
+    @Test
+    public void testDescriptorExhaustion() throws IOException, TimeoutException, InterruptedException {
+        RemoteJvmProcess process = itJar.runClass(TreeNodeWriter.class.getName(), PROPS, "5000000");
+        process.waitForMessage("go", 600);
+        process.setDescriptorLimit(1);
+        process.waitForFinish();
+    }
+
+}
