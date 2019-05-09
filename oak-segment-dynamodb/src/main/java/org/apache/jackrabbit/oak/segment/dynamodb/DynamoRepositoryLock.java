13cf62fbd8d6aa662cb545c05c4d10f8db4d298b
Tomek Rękawek <rekawek@adobe.com>
2019-05-09 10:00:12 +0200

    Temporarily removed the locks, to avoid version conflict in OSGi.

--- oak-segment-dynamodb/src/main/java/org/apache/jackrabbit/oak/segment/dynamodb/DynamoRepositoryLock.java
+++ oak-segment-dynamodb/src/main/java/org/apache/jackrabbit/oak/segment/dynamodb/DynamoRepositoryLock.java
@@ -16,59 +16,18 @@
  */
 package org.apache.jackrabbit.oak.segment.dynamodb;
 
-import com.amazonaws.services.dynamodbv2.AcquireLockOptions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBLockClient;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBLockClientOptions;
-import com.amazonaws.services.dynamodbv2.CreateDynamoDBTableOptions;
-import com.amazonaws.services.dynamodbv2.LockItem;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.AmazonDynamoDBException;
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import org.apache.jackrabbit.oak.segment.spi.persistence.RepositoryLock;
 
 import java.io.IOException;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 public class DynamoRepositoryLock implements RepositoryLock {
 
-    private final AmazonDynamoDBLockClient client;
-
-    private final LockItem lock;
-
     public DynamoRepositoryLock(AmazonDynamoDB amazonDynamoDB, String tableName) throws IOException {
-        try {
-            DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
-            if (!DynamoUtils.tableExists(dynamoDB, tableName)) {
-                AmazonDynamoDBLockClient.createLockTableInDynamoDB(CreateDynamoDBTableOptions.builder(
-                        amazonDynamoDB,
-                        new ProvisionedThroughput(10L, 10L),
-                        tableName
-                ).build());
-                dynamoDB.getTable(tableName).waitForActive();
-            }
-            client = new AmazonDynamoDBLockClient(
-                    AmazonDynamoDBLockClientOptions.builder(amazonDynamoDB, tableName)
-                            .withTimeUnit(TimeUnit.SECONDS)
-                            .withLeaseDuration(10L)
-                            .withHeartbeatPeriod(3L)
-                            .withCreateHeartbeatBackgroundThread(true)
-                            .build());
-            Optional<LockItem> lockItem = client.tryAcquireLock(AcquireLockOptions.builder("key").build());
-            lock = lockItem.orElseThrow(() -> new IOException("Can't acquire lock"));
-        } catch (AmazonDynamoDBException | InterruptedException e) {
-            throw new IOException(e);
-        }
     }
 
     @Override
     public void unlock() throws IOException {
-        try {
-            client.releaseLock(lock);
-        } catch (AmazonDynamoDBException e) {
-            throw new IOException(e);
-        }
+
     }
 }
