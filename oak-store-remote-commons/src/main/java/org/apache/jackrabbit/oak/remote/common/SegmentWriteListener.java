e4666f4b2b8a761bbc4d5de1ed86b426b92f92db
Tomek Rękawek <rekawek@adobe.com>
2019-09-04 16:27:01 +0200

    Deal with exceptions in the SegmentWriteListener.

--- oak-store-remote-commons/src/main/java/org/apache/jackrabbit/oak/remote/common/SegmentWriteListener.java
+++ oak-store-remote-commons/src/main/java/org/apache/jackrabbit/oak/remote/common/SegmentWriteListener.java
@@ -2,11 +2,15 @@ package org.apache.jackrabbit.oak.remote.common;
 
 import org.apache.jackrabbit.oak.remote.proto.SegmentProtos;
 import org.apache.jackrabbit.oak.segment.spi.monitor.IOMonitorAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.function.Consumer;
 
 public class SegmentWriteListener extends IOMonitorAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(SegmentWriteListener.class);
 
     private Consumer<SegmentProtos.SegmentBlob> delegate = s -> {};
 
@@ -18,7 +22,12 @@ public class SegmentWriteListener extends IOMonitorAdapter {
                 .getSegmentIdBuilder()
                     .setMsb(msb)
                     .setLsb(lsb);
-        delegate.accept(builder.build());
+
+        try {
+            delegate.accept(builder.build());
+        } catch (Exception e) {
+            log.error("Can't pass the blob to delegate", e);
+        }
     }
 
     public void setDelegate(Consumer<SegmentProtos.SegmentBlob> delegate) {
